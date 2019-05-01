ea21419932a0cc2c4677ccfeec948bed491211ce
Mustafa <zubaide@aol.com>
2019-04-30 22:25:28 -0700

    First react push

---
+++ precache-manifest.255d939d98feb0069ff82299a90ab97f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "851bbf5549fd709c731f8b6a5e552d43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f55687645ca2f9bf4a50",
+    "url": "/static/css/main.db2f91af.chunk.css"
+  },
+  {
+    "revision": "94c3165186f215582bb2",
+    "url": "/static/js/2.c37d2ede.chunk.js"
+  },
+  {
+    "revision": "f55687645ca2f9bf4a50",
+    "url": "/static/js/main.4239f529.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
